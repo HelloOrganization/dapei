f42f89c398eaabb8cc10969fbd9f380b6d911e27
CQY <pku_cqy@126.com>
2015-03-24 18:58:04 +0800

    remove image pipeline

--- tmall/tmall/pipelines.py
+++ tmall/tmall/pipelines.py
@@ -17,19 +17,19 @@ class TmallPipeline(object):
         return item
 
 
-class MyImagesPipeline(ImagesPipeline):
-    def get_media_requests(self, item, info):
-        for image_url in item['image_urls']:
-            yield Request(image_url)
-
-    def item_completed(self, results, item, info):
-    
-        image_paths = [x['path'] for ok, x in results if ok]
-    
-        if not image_paths:
-    
-            raise DropItem("Item contains no images")
-    
-        item['image_paths'] = image_paths
-    
-        return item
+#class MyImagesPipeline(ImagesPipeline):
+#    def get_media_requests(self, item, info):
+#        for image_url in item['image_urls']:
+#            yield Request(image_url)
+#
+#    def item_completed(self, results, item, info):
+#    
+#        image_paths = [x['path'] for ok, x in results if ok]
+#    
+#        if not image_paths:
+#    
+#            raise DropItem("Item contains no images")
+#    
+#        item['image_paths'] = image_paths
+#    
+#        return item
